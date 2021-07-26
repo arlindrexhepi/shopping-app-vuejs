1985f3bc8b06128ca919d3114c5ff6e22cc9168f
arlindrexhepi <axr3649@g.rit.edu>
2021-07-26 17:03:59 +0200

    smallchanges

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="container mt-4">
-    <h1 class="animate__animated animate__fadeInLeft">This is an about page</h1>
-    <button class="btn btn-primary">Hello Bootstrap</button>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <h1 class="animate__animated animate__fadeInLeft">This will be the checkout page!</h1>
   </div>
 </template>
